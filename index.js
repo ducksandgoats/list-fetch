2e482db3a1212416b6261e88c4470f5f8ddfeb77
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-01 18:40:24 -0600

    update package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = async function makeBTFetch (opts = {}) {
-  const makeFetch = require('make-fetch')
+  const {makeFetch} = await import('make-fetch')
   const streamToIterator = require('stream-async-iterator')
   const mime = require('mime/lite')
   const parseRange = require('range-parser')
